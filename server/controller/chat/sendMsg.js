f28a683a11edf1a7f704c21043d3ca8ad0f26568
Md Mubin <fontdevmubin@gmail.com>
2025-04-24 23:54:50 +0600

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   server/controller/chat/sendMsg.js 	modified:   server/package-lock.json 	modified:   server/package.json

--- server/controller/chat/sendMsg.js
+++ server/controller/chat/sendMsg.js
@@ -5,18 +5,22 @@ const sendMsg = async (req,res)=>{
 
     const {reciverID, content, conversationID} = req.body
 
+    // if there is no reciver ID
     if(!reciverID){
         return res.status(400).send("Something went wrong")
     }
 
+    // if there is no content
     if(!content){
         return res.status(400).send("Something went wrong")
     }
 
+    // if there is no conversation ID
     if(!conversationID){
         return res.status(400).send("Something went wrong")
     }
 
+    // create new massage
     const newMsg = new chatSchema({
         sender : req.user.id,
         reciver : reciverID,
@@ -26,6 +30,7 @@ const sendMsg = async (req,res)=>{
 
     newMsg.save()
 
+    // send last msg to conversation list schema
     await convoSchema.findByIdAndUpdate(conversationID, {lastMsg : newMsg})
 
     return res.status(200).send(newMsg)
