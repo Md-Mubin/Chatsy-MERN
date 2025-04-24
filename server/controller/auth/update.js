ee8759f9b027fbf988565d43b72a2debda19ba07
Md Mubin <fontdevmubin@gmail.com>
2025-04-24 23:57:43 +0600

    Changes to be committed: 	modified:   server/controller/auth/update.js

--- server/controller/auth/update.js
+++ server/controller/auth/update.js
@@ -6,21 +6,26 @@ const update = async (req, res) => {
 
     const { name, pass } = req.body
 
-    const existUser = await userSchema.findById(req.user.id)
+    try {
+        // if user exists
+        const existUser = await userSchema.findById(req.user.id)
 
-    if (name) existUser.name = name.trim();
-    if (pass) existUser.pass = pass;
+        if (name) existUser.name = name.trim();
+        if (pass) existUser.pass = pass;
 
-    if (req.file.path) {
-        await cloudinary.uploader.destroy(existUser.avatar.split('/').pop().split('.')[0])
-        const result = await cloudinary.uploader.upload(req.file.path)
-        existUser.avatar = result.url
-        fs.unlinkSync(req.file.path)
+        if (req.file.path) {
+            await cloudinary.uploader.destroy(existUser.avatar.split('/').pop().split('.')[0])
+            const result = await cloudinary.uploader.upload(req.file.path)
+            existUser.avatar = result.url
+            fs.unlinkSync(req.file.path)
+        }
+
+        existUser.save()
+
+        return res.status(200).send({ msg: "Update Successfull" }, existUser)
+    } catch (error) {
+        return res.status(500).send("Server Error")
     }
-
-    existUser.save()
-
-    return res.status(200).send({ msg: "Update Successfull" }, existUser)
 }
 
 module.exports = update
