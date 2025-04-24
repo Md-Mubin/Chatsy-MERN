a0fe2b27fb8640bf3cc745b6bf0df8756f5ded38
Md Mubin <fontdevmubin@gmail.com>
2025-04-24 23:56:55 +0600

    Changes to be committed: 	modified:   server/controller/auth/verifyEmail.js

--- server/controller/auth/verifyEmail.js
+++ server/controller/auth/verifyEmail.js
@@ -4,17 +4,19 @@ const verifyEmail = async (req, res) => {
 
     const { email, OTP } = req.body
 
+    // check if email / OTP is inputed
     if (!email || !OTP) return res.status(400).send("Invalid Request")
 
+    // varifing
     const verified_ID = await userSchema.findOne({ email, OTP, OTP_expire: { $gt: Date.now() } })
 
-    if(!verified_ID) return res.status(400).send("Invalid OTP")
+    if (!verified_ID) return res.status(400).send("Invalid OTP")
 
-        verified_ID.OTP = null,
+    verified_ID.OTP = null,
         verified_ID.OTP_expire = null,
         verified_ID.isVerified = true,
         verified_ID.save()
-        res.status(200).send("Verified Successfull")
+    res.status(200).send("Verified Successfull")
 }
 
 module.exports = verifyEmail
