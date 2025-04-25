a9127d0f03bda5460b851b606b4f2ab8818e3b91
Md Mubin <fontdevmubin@gmail.com>
2025-04-25 23:48:16 +0600

    Changes to be committed: 	modified:   server/controller/chat/convoList.js

--- server/controller/chat/convoList.js
+++ server/controller/chat/convoList.js
@@ -1,23 +1,27 @@
 const convoSchema = require("../../models/convoSchema")
 
-const convoList = async (req,res)=>{
+const convoList = async (req, res) => {
 
-    const conversation = await convoSchema.find({
-        $or : [
-            {
-                creator : req.user.id
-            },
-            {
-                participent : req.user.id
-            }
-        ]
-    }).populate("creator", "name avatar email").populate("participent", "name avatar email").populate("lastMsg")
+    try {
+        const conversation = await convoSchema.find({
+            $or: [
+                {
+                    creator: req.user.id
+                },
+                {
+                    participent: req.user.id
+                }
+            ]
+        }).populate("creator", "name avatar email").populate("participent", "name avatar email").populate("lastMsg")
 
-    if(!conversation){
-        return res.status(400).send("No Coversation Found")
+        if (!conversation) {
+            return res.status(400).send("No Coversation Found")
+        }
+
+        res.status(200).send(conversation)
+    } catch (error) {
+        return res.status(500).send("Server Error")
     }
-
-    res.status(200).send(conversation)
 }
 
 module.exports = convoList
