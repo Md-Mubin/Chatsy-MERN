27589b5322c3c1766e178800fe26bb337b11c7fa
Md Mubin <fontdevmubin@gmail.com>
2025-04-25 23:47:33 +0600

    Changes to be committed: 	modified:   server/controller/chat/getMsg.js

--- server/controller/chat/getMsg.js
+++ server/controller/chat/getMsg.js
@@ -1,20 +1,24 @@
 const chatSchema = require("../../models/chatSchema")
 
-const getMsg = async (req,res)=>{
+const getMsg = async (req, res) => {
 
-    const {conversationID} = req.params
+    try {
+        const { conversationID } = req.params
 
-    if(!conversationID){
-        return res.status(400).send("No massage foound")
+        if (!conversationID) {
+            return res.status(400).send("No massage foound")
+        }
+
+        const getMassage = await chatSchema.find({ conversations: conversationID })
+
+        if (!getMassage) {
+            return res.status(400).send("No massage foound")
+        }
+
+        res.status(200).send(getMassage)
+    } catch (error) {
+        return res.status(500).send("Server Error")
     }
-
-    const getMassage = await chatSchema.find({conversations : conversationID})
-
-    if(!getMassage){
-        return res.status(400).send("No massage foound")
-    }
-
-    res.status(200).send(getMassage)
 }
 
 module.exports = getMsg
