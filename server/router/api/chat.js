561bbbce9b0c1100ae919dad742011e2f20c1744
Md Mubin <fontdevmubin@gmail.com>
2025-04-25 23:46:07 +0600

    Changes to be committed: 	modified:   server/router/api/chat.js

--- server/router/api/chat.js
+++ server/router/api/chat.js
@@ -3,12 +3,13 @@ const coversation = require("../../controller/chat/conversation")
 const authMiddleware = require("../../middleware/authMiddleware")
 const convoList = require("../../controller/chat/convoList")
 const sendMsg = require("../../controller/chat/sendMsg")
+const getMsg = require("../../controller/chat/getMsg")
 const chatRouter = express.Router()
 
 chatRouter.post("/createConvo",authMiddleware, coversation)
 chatRouter.post("/convoList",authMiddleware, convoList)
 
-chatRouter.post("/send", authMiddleware, sendMsg)
-// chatRouter.get("/send", )
+chatRouter.post("/sendMsg", authMiddleware, sendMsg)
+chatRouter.get("/getMsg/:conversationID", authMiddleware, getMsg )
 
 module.exports = chatRouter
