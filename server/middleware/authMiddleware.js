b996a1b9684702de79d02fcacf456b4793fd11c3
Md Mubin <fontdevmubin@gmail.com>
2025-04-25 23:49:49 +0600

    fixed some issues project keep going

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,19 +2,23 @@ const jwt = require("jsonwebtoken")
 
 const authMiddleware = (req, res, next) => {
 
-    const token = req.header("authorization")
-    if (token) {
-        jwt.verify(token, process.env.SECRET_ACC_TOKEN, function (err, decoded) {
-            if (err) {
-               return res.status(400).send({msg : "Bad Request"})
-            }
-            if (decoded.data) {
-                req.user = decoded.data
-                next()
-            }
-        })
-    }else{
-        return res.status(400).send({msg : "Bad Request"})
+    try {
+        const token = req.header("authorization")
+        if (token) {
+            jwt.verify(token, process.env.SECRET_ACC_TOKEN, function (err, decoded) {
+                if (err) {
+                    return res.status(400).send({ msg: "Bad Request" })
+                }
+                if (decoded.data) {
+                    req.user = decoded.data
+                    next()
+                }
+            })
+        } else {
+            return res.status(400).send({ msg: "Bad Request" })
+        }
+    } catch (error) {
+        return res.status(500).send("Server Error")
     }
 }
 
