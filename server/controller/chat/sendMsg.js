ffa4f77554b78f5d9e5a9b8b379bcc82f74f4175
Md Mubin <fontdevmubin@gmail.com>
2025-04-25 23:45:23 +0600

    Changes to be committed: 	modified:   server/controller/chat/sendMsg.js

--- server/controller/chat/sendMsg.js
+++ server/controller/chat/sendMsg.js
@@ -1,45 +1,49 @@
 const chatSchema = require("../../models/chatSchema")
 const convoSchema = require("../../models/convoSchema")
 
-const sendMsg = async (req,res)=>{
+const sendMsg = async (req, res) => {
 
-    const {reciverID, content, conversationID} = req.body
+    try {
+        const { reciverID, content, conversationID } = req.body
 
-    // if there is no reciver ID
-    if(!reciverID){
-        return res.status(400).send("Something went wrong")
+        // if there is no reciver ID
+        if (!reciverID) {
+            return res.status(400).send("Something went wrong")
+        }
+
+        // if there is no content
+        if (!content) {
+            return res.status(400).send("Something went wrong")
+        }
+
+        // if there is no conversation ID
+        if (!conversationID) {
+            return res.status(400).send("Something went wrong")
+        }
+
+        const existChat = await chatSchema.findOne({ _id: conversationID })
+
+        if (!existChat) {
+            return res.status(400).send("No Conversation Found")
+        }
+
+        // create new massage
+        const newMsg = new chatSchema({
+            sender: req.user.id,
+            reciver: reciverID,
+            content,
+            conversations: existChat._id
+        })
+
+        newMsg.save()
+
+        // send last msg to conversation list schema
+        await convoSchema.findByIdAndUpdate(existChat._id, { lastMsg: newMsg })
+
+        return res.status(200).send(newMsg)
+    } catch (error) {
+        return res.status(500).send("Server Error")
     }
-
-    // if there is no content
-    if(!content){
-        return res.status(400).send("Something went wrong")
-    }
-
-    // if there is no conversation ID
-    if(!conversationID){
-        return res.status(400).send("Something went wrong")
-    }
-
-    const existChat = await chatSchema.findOne({_id : conversationID})
-
-    if(!existChat){
-        return res.status(400).send("No Conversation Found")
-    }
-
-    // create new massage
-    const newMsg = new chatSchema({
-        sender : req.user.id,
-        reciver : reciverID,
-        content,
-        conversations : existChat._id
-    })
-
-    newMsg.save()
-
-    // send last msg to conversation list schema
-    await convoSchema.findByIdAndUpdate(existChat._id, {lastMsg : newMsg})
-
-    return res.status(200).send(newMsg)
 }
 
 module.exports = sendMsg
