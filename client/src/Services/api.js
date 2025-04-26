d4344d0e7c24d0cf92c4e8d25b8ad59b052623b0
Md Mubin <fontdevmubin@gmail.com>
2025-04-26 23:06:41 +0600

    Changes to be committed: 	modified:   client/package-lock.json 	modified:   client/package.json 	new file:   client/src/Services/api.js

---
+++ client/src/Services/api.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL_API,
+    headers: {
+        "Content-Type": "application/api"
+    }
+})
+
+export const authoraizations = {
+    registration: async (registerData) => {
+        const res = await api.post("/auth/register", registerData)
+        return res.data
+    },
+
+    login: async (loginData) => {
+        const res = await api.post("/auth/login", loginData)
+        return res.data
+    }
+} 
