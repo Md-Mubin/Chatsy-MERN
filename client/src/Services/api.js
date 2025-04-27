ef6c7d8f9b5b72c26b9bbc23adbd6f4adfa83caf
Md Mubin <fontdevmubin@gmail.com>
2025-04-27 23:58:58 +0600

    Changes to be committed: 	modified:   client/src/Services/api.js

--- client/src/Services/api.js
+++ client/src/Services/api.js
@@ -8,16 +8,19 @@ const api = axios.create({
 })
 
 export const authoraizations = {
+    // for register
     registration: async (registerData) => {
         const res = await api.post("/auth/register", registerData)
         return res.data
     },
 
+    // for login
     login: async (loginData) => {
         const res = await api.post("/auth/login", loginData)
         return res.data
     },
 
+    // for otp to verify email
     emailVerify : async(otpData)=>{
         const res = await api.post("/auth/emailVarified", otpData)
         return res.data
