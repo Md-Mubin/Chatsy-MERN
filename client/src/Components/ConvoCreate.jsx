cd938deb047ffb0db38057f71956a0d3625090ec
Md Mubin <fontdevmubin@gmail.com>
2025-05-04 23:53:35 +0600

    Changes to be committed: 	new file:   client/src/Components/ConvoCreate.jsx

---
+++ client/src/Components/ConvoCreate.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ConvoCreate = () => {
+    return (
+        <>
+            <section>
+                
+            </section>
+        </>
+    )
+}
+
+export default ConvoCreate
