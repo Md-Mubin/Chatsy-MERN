922601519708600cc9a602a98ab8d75536fdc3e7
Md Mubin <fontdevmubin@gmail.com>
2025-05-04 23:54:18 +0600

    Changes to be committed: 	modified:   client/src/Components/ConvoCreate.jsx

--- client/src/Components/ConvoCreate.jsx
+++ client/src/Components/ConvoCreate.jsx
@@ -4,7 +4,18 @@ const ConvoCreate = () => {
     return (
         <>
             <section>
-                
+                <ul>
+                    <li>
+                        <h1>Chatsy</h1>
+                    </li>
+
+                    <li>
+                        <form>
+                            <input type="email" />
+                            <button>Add</button>
+                        </form>
+                    </li>
+                </ul>
             </section>
         </>
     )
