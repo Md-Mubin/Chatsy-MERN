8ab12794b88aed10185af6167dfe107a20f6c5e0
Md Mubin <fontdevmubin@gmail.com>
2025-05-05 23:05:24 +0600

    modified:   client/src/App.css 	modified:   client/src/Components/ConvoList.jsx

--- client/src/Components/ConvoList.jsx
+++ client/src/Components/ConvoList.jsx
@@ -20,7 +20,7 @@ const ConvoList = () => {
                     chatUsers.forEach((items) => {
                         if (items.creator._id === loggedUserData._id) {
                             arr.push(items.participent)
-                        }else if(items.participent._id === loggedUserData._id){
+                        } else if (items.participent._id === loggedUserData._id) {
                             arr.push(items.creator)
                         }
                     });
@@ -36,17 +36,24 @@ const ConvoList = () => {
     return (
         <>
             <section className='convoListSec'>
-                <ul className='px-2'>
                     {
-                        allChatUser.length === 0 
-                        ? <li>No Conversation Found</li>
-                        : allChatUser.map((datas)=>(
-                            <li key={datas._id}>
-                                {datas.name}
-                            </li>
-                        ))
+                        allChatUser.length === 0
+                            ? <p>No Conversation Found</p>
+                            : allChatUser.map((datas) => (
+                                <ul key={datas._id} className='flex items-center gap-5 mx-4'>
+                                    <li className='w-[50px] h-[50px] rounded-full bg-[#ffffff17] flex justify-center items-center'>
+                                        {
+                                            datas.avatar 
+                                            ? <img src={datas.avatar} alt="" />
+                                            : <div>{datas.name.split("")[0]}</div>
+                                        }
+                                    </li>
+                                    <li>
+                                        {datas.name}
+                                    </li>
+                                </ul>
+                            ))
                     }
-                </ul>
             </section>
         </>
     )
