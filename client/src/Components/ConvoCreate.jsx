045a98778b50cc3a9fb7cd609c170b5bd967403d
Md Mubin <fontdevmubin@gmail.com>
2025-05-09 23:31:39 +0600

    Changes to be committed: 	modified:   client/src/Components/ConvoCreate.jsx

--- client/src/Components/ConvoCreate.jsx
+++ client/src/Components/ConvoCreate.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { chattings } from '../Services/api'
 import { useDispatch } from 'react-redux'
 import { getConvoList } from '../Store/Slices/convoListSlice'
