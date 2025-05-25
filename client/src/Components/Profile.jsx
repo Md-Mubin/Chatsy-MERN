3370330174ed223e65b411874b1c64a84e455706
Md Mubin <fontdevmubin@gmail.com>
2025-05-25 14:03:57 +0600

    modified:   client/src/App.css 	modified:   client/src/Components/Profile.jsx

--- client/src/Components/Profile.jsx
+++ client/src/Components/Profile.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+
+  const { user } = useSelector((state) => state.loggedUserData)
+
   return (
-    <div>Profile</div>
+    <>
+      <section className='w-full h-[100dvh] flex justify-center items-center'>
+        <ul className='py-10 w-[400px] border-2 border-[#515257] text-center'>
+          <li className='text-3xl text-[#]'>{user?.name}</li>
+        </ul>
+      </section>
+    </>
   )
 }
 
