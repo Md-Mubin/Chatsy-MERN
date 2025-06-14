f6f0aaf0af57f457f714027a8136ba41cb736914
Md Mubin <fontdevmubin@gmail.com>
2025-06-14 23:45:19 +0600

    --

--- client/src/Pages/BlockPage.jsx
+++ client/src/Pages/BlockPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import BlockList from '../Components/BlockList'
+import Chats from '../Components/Chats'
 
 const BlockPage = () => {
 
