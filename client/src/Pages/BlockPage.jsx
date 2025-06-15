d16fa7f64d92d31bd909c8ea03ad3cde14d3ca72
Md Mubin <fontdevmubin@gmail.com>
2025-06-15 14:09:09 +0600

    __

--- client/src/Pages/BlockPage.jsx
+++ client/src/Pages/BlockPage.jsx
@@ -6,19 +6,15 @@ import Chats from '../Components/Chats'
 const BlockPage = () => {
 
     const { selectedConvo } = useSelector((state) => state.convoListSlice)
- 
+
     return (
         <>
             <div className='flex w-full'>
                 <ul className='pt-4 px-2 border-r-2 border-[#515257] '>
                     <li>
                         <h1 className='font-normal text-5xl text-[#88d4ca]'>Chatsy</h1>
-                    </li>
-                    <li>
                         <BlockList />
                     </li>
-                </ul>
-                <ul>
                     {
                         selectedConvo
                             ?
