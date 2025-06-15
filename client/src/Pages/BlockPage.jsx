6fc1f7cc62178982f42a10aa5ab153a1b7ad5ecf
Md Mubin <fontdevmubin@gmail.com>
2025-06-15 14:11:11 +0600

    __

--- client/src/Pages/BlockPage.jsx
+++ client/src/Pages/BlockPage.jsx
@@ -9,25 +9,23 @@ const BlockPage = () => {
 
     return (
         <>
-            <div className='flex w-full'>
-                <ul className='pt-4 px-2 border-r-2 border-[#515257] '>
-                    <li>
-                        <h1 className='font-normal text-5xl text-[#88d4ca]'>Chatsy</h1>
-                        <BlockList />
-                    </li>
-                    {
-                        selectedConvo
-                            ?
-                            <li className='w-full hidden lg:block'>
-                                <Chats />
-                            </li>
-                            :
-                            <li className=' justify-center items-center w-full h-full hidden lg:flex'>
-                                <h3 className='text-3xl xl:text-4xl text-[#515257] text-center'>Please Select A Chat to see the conversation</h3>
-                            </li>
-                    }
-                </ul>
-            </div>
+            <ul className='flex w-full'>
+                <li className='pt-4 px-2 border-r-2 border-[#515257]'>
+                    <h1 className='font-normal text-5xl text-[#88d4ca]'>Chatsy</h1>
+                    <BlockList />
+                </li>
+                {
+                    selectedConvo
+                        ?
+                        <li className='w-full hidden lg:block'>
+                            <Chats />
+                        </li>
+                        :
+                        <li className=' justify-center items-center w-full h-full hidden lg:flex'>
+                            <h3 className='text-3xl xl:text-4xl text-[#515257] text-center'>Please Select A Chat to see the conversation</h3>
+                        </li>
+                }
+            </ul>
         </>
     )
 }
